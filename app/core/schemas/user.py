60c19980aea8f87cf2f1bf5470c66193c75edbaa
Nurba2405 <nurbekorazov@gmail.com>
2025-01-13 16:58:47 +0500

    Добавляю, чтобы сохранить все файлы, ошибка осталась, но файлы хочу запушить

--- app/core/schemas/user.py
+++ app/core/schemas/user.py
@@ -1,10 +1,24 @@
+from typing import Optional
 from pydantic import BaseModel, EmailStr
-
+# Схема Pydantic
 # create, update, read (get)
 
 
 class UserCreate(BaseModel):
     name: str
     password: str
-    email: EmailStr
+    email: EmailStr # user@gmail.com
+    age: Optional[int] = None # Опциональные это не обязательно
 
+class UserRead(BaseModel):
+    id: int
+    name: str
+    password: str
+    email: EmailStr # user@gmail.com
+    age: Optional[int] = None # Опциональные это не обязательно
+
+class UserUpdate(BaseModel):
+    name: Optional[str] = None # обновляется одно только меняется
+    password: Optional[str] = None
+    email: Optional[EmailStr] = None # user@gmail.com
+    age: Optional[int] = None # Опциональные это не обязательно
